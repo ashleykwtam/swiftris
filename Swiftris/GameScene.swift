a5371f60f8a27dec04f30b77d57d1402af7bb581
Ashley <ashleykwtam@gmail.com>
2014-10-10 14:34:58 -0400

    implemented methods for the drawing layer to display sprites

--- Swiftris/GameScene.swift
+++ Swiftris/GameScene.swift
@@ -8,10 +8,19 @@
 
 import SpriteKit
 
+// define point size of each block sprite: 20.0 x 20.0
+let BlockSize:CGFloat = 20.0
+
 // represents the slowest speed at which shapes will travel
 let TickLengthLevelOne = NSTimeInterval(600)
 
 class GameScene: SKScene {
+    
+    // SKNodes -> superimposed layers of activity within scene
+    // gameLayer sits above background visuals and shapeLayer on top of that
+    let gameLayer = SKNode()
+    let shapeLayer = SKNode()
+    let LayerPosition = CGPoint(x: 6, y: -6)
     
     // tickLengthMillis set to TickLengthLevelOne by default
     // lastTick will track last time a tick is experienced
@@ -20,7 +29,9 @@ class GameScene: SKScene {
     var tickLengthMillis = TickLengthLevelOne
     var lastTick:NSDate?
     
-    required init(coder aDecoder: NSCoder!){
+    var textureCache = Dictionary<String, SKTexture>()
+    
+    required init(coder aDecoder: NSCoder){
         fatalError("NSCoder not supported")
     }
     
@@ -33,6 +44,17 @@ class GameScene: SKScene {
         background.position = CGPoint(x: 0, y: 0)
         background.anchorPoint = CGPoint(x: 0, y: 1.0)
         addChild(background)
+        
+        addChild(gameLayer)
+        
+        let gameBoardTexture = SKTexture(imageNamed: "background")
+        let gameBoard = SKSpriteNode(texture: gameBoardTexture, size: CGSizeMake(BlockSize * CGFloat(NumColumns), BlockSize * CGFloat(NumRows)))
+        gameBoard.anchorPoint = CGPoint(x: 0, y: 1.0)
+        gameBoard.position = LayerPosition
+        
+        shapeLayer.position = LayerPosition
+        shapeLayer.addChild(gameBoard)
+        gameBoard.addChild(shapeLayer)
     }
    
     override func update(currentTime: CFTimeInterval) {
@@ -60,5 +82,70 @@ class GameScene: SKScene {
     
     func stopTicking(){
         lastTick = nil
+    }
+    
+    // function returns precise coordinate on screen for where a block sprite belongs based on row and column position
+    // need to find its center coordinate before placing it in shapeLayer object
+    func pointForColumn(column: Int, row: Int) -> CGPoint {
+        let x: CGFloat = LayerPosition.x + (CGFloat(column) * BlockSize) + (BlockSize / 2)
+        let y: CGFloat = LayerPosition.y - (CGFloat(row) * BlockSize) + (BlockSize / 2)
+        return CGPointMake(x, y)
+    }
+    
+    
+    func addPreviewShapeToScene(shape:Shape, completion:() -> ()) {
+        for (idx, block) in enumerate(shape.blocks) {
+            
+            // create method that will add a shape to the scene as a preview shape
+            // dictionary used to store copies of reusable SKTexture objects
+            var texture = textureCache[block.spriteName]
+            if texture == nil {
+                texture = SKTexture(imageNamed: block.spriteName)
+                textureCache[block.spriteName] = texture
+            }
+            let sprite = SKSpriteNode(texture: texture)
+            
+            // use pointForColumn method to place each block's sprite in proper location
+            // start it at row - 2, such that preview piece animates smoothly into place
+            sprite.position = pointForColumn(block.column, row: block.row - 2)
+            shapeLayer.addChild(sprite)
+            block.sprite = sprite
+            
+            sprite.alpha = 0
+            
+            // introduce SKAction objects which are responsible for visually manipulating SKNode objects
+            // each block fades and moves into place as it appears as part of the next piece
+            // move two rows down and fade to 70% opacity
+            let moveAction = SKAction.moveTo(pointForColumn(block.column, row: block.row), duration: NSTimeInterval(0.2))
+            moveAction.timingMode = .EaseOut
+            let fadeInAction = SKAction.fadeAlphaTo(0.7, duration: 0.4)
+            fadeInAction.timingMode = .EaseOut
+            sprite.runAction(SKAction.group([moveAction, fadeInAction]))
+        }
+        runAction(SKAction.waitForDuration(0.4), completion: completion)
+    }
+    
+    // movePreviewShape and redrawShape makes use of SKAction objects to move and redraw given shape
+    
+    func movePreviewShape(shape:Shape, completion:() -> ()) {
+        for (idx, block) in enumerate(shape.blocks) {
+            let sprite = block.sprite!
+            let moveTo = pointForColumn(block.column, row:block.row)
+            let moveToAction:SKAction = SKAction.moveTo(moveTo, duration: 0.2)
+            moveToAction.timingMode = .EaseOut
+            sprite.runAction(SKAction.group([moveToAction, SKAction.fadeAlphaTo(1.0, duration: 0.2)]), completion: nil)
+        }
+        runAction(SKAction.waitForDuration(0.2), completion: completion)
+    }
+    
+    func redrawShape(shape:Shape, completion:() -> ()) {
+        for (idx, block) in enumerate(shape.blocks) {
+            let sprite = block.sprite!
+            let moveTo = pointForColumn(block.column, row:block.row)
+            let moveToAction:SKAction = SKAction.moveTo(moveTo, duration: 0.05)
+            moveToAction.timingMode = .EaseOut
+            sprite.runAction(moveToAction, completion: nil)
+        }
+        runAction(SKAction.waitForDuration(0.05), completion: completion)
     }
 }
