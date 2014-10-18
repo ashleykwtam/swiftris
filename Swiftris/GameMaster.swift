04e47026f6a652349bcd03cd1243954fc75c2feb
Ashley <ashleykwtam@gmail.com>
2014-10-18 02:39:09 -0400

    added scoring and levelling up

--- Swiftris/GameMaster.swift
+++ Swiftris/GameMaster.swift
@@ -15,6 +15,9 @@ let StartingRow = 0
 let PreviewColumn = 12
 let PreviewRow = 1
 
+let PointsPerLine = 10
+let LevelThreshold = 1000
+
 protocol GameMasterDelegate {
     func gameDidEnd(gamemaster: GameMaster)
     func gameDidBegin(gamemaster: GameMaster)
@@ -32,7 +35,12 @@ class GameMaster {
     //GameViewController will implement and attach itself as delegate in order to update UI and react to game state changes
     var delegate:GameMasterDelegate?
     
+    var score:Int
+    var level:Int
+    
     init() {
+        score = 0
+        level = 1
         fallingShape = nil
         nextShape = nil
         blockArray = Array2D<Block>(columns: NumColumns, rows: NumRows)
@@ -102,7 +110,71 @@ class GameMaster {
     }
     
     func endGame() {
+        score = 0
+        level = 1
         delegate?.gameDidEnd(self)
+    }
+    
+    // function which returns tuple (two arrays of linesRemoved and fallenBlocks)
+    // linesRemoved maintains each row of blocks user has filled in completely
+    func removeCompletedLines() -> (linesRemoved: Array<Array<Block>>, fallenBlocks: Array<Array<Block>>) {
+        var removedLines = Array<Array<Block>>()
+        for var row = NumRows - 1; row > 0; row-- {
+            var rowOfBlocks = Array<Block>()
+            
+            // iterates from 0-9, adds every block in given row to local array variable named rowOfBlocks
+            // if it has 10 blocks in total, it counts that as a removed line and adds it to the return variable
+            for column in 0..<NumColumns {
+                if let block = blockArray[column, row] {
+                    rowOfBlocks.append(block)
+                }
+            }
+            if rowOfBlocks.count == NumColumns {
+                removedLines.append(rowOfBlocks)
+                for block in rowOfBlocks {
+                    blockArray[block.column, block.row] = nil
+                }
+            }
+        }
+        
+        // check to see if we have recovered any lines at all, if not, return empty arrays
+        if removedLines.count == 0 {
+            return ([], [])
+        }
+        
+        // add points to player's score based on # of lines they've created and their level
+        // if points exceed level*1000, level up
+        let pointsEarned = removedLines.count * PointsPerLine * level
+        score += pointsEarned
+        if score >= level * LevelThreshold {
+            level += 1
+            delegate?.gameDidLevelUp(self)
+        }
+        
+        var fallenBlocks = Array<Array<Block>>()
+        for column in 0..<NumColumns {
+            var fallenBlocksArray = Array<Block>()
+            
+            // starting in left-most column above bottom-most removed line, count upwards towards top of game board
+            // take each remaining block on gameboard and lower it as far as possible
+            // fallenBlocks is an array of arrays, each sub-array is filled with blocks that fell to a new position as a result of cleared lines
+            for var row = removedLines[0][0].row - 1; row > 0; row-- {
+                if let block = blockArray[column, row] {
+                    var newRow = row
+                    while (newRow < NumRows - 1 && blockArray[column, newRow + 1] == nil) {
+                        newRow++
+                    }
+                    block.row = newRow
+                    blockArray[column, row] = nil
+                    blockArray[column, newRow] = block
+                    fallenBlocksArray.append(block)
+                }
+            }
+            if fallenBlocksArray.count > 0 {
+                fallenBlocks.append(fallenBlocksArray)
+            }
+        }
+        return (removedLines, fallenBlocks)
     }
     
     // conveience function provided to drop a shape to bottom of game board
